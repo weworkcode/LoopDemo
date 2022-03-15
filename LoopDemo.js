25d940e079ee92ec1cc2ac7a2bcee77c8fda1e19
weworkcode <90383460+weworkcode@users.noreply.github.com>
2022-03-15 14:52:25 -0700

    Create LoopDemo.js

---
+++ LoopDemo.js
@@ -0,0 +1,8 @@
+public class LoopDemo {
+    public static void main(String[] args) {
+    for(int num = 0; num < 100; num++){
+        if (num%5 == 0 && num%6 ==0)
+        System.out.println(num);
+    }
+    }
+}
